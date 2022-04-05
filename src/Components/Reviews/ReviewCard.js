983ea78bab1c0555ef07c7fe2c1cb200424878fb
Mahathir-Gunjon <mahathirgunjon@gmail.com>
2022-04-05 15:57:51 +0600

    destructuring was missed and fixed

--- src/Components/Reviews/ReviewCard.js
+++ src/Components/Reviews/ReviewCard.js
@@ -3,19 +3,20 @@ import React from 'react';
 import './ReviewCard.css'
 
 const ReviewCard = ({ review }) => {
+    const { name, rating, reviewText, picture } = review;
     return (
         <div>
             <div className="md:flex justify-between my-2 md:my-10 card-info shadow-xl">
                 <div className="mr-3 md:mr-10">
-                    <img className='img-sizing' src={review.picture} alt="" />
+                    <img className='img-sizing' src={picture} alt="" />
                 </div>
                 <div className="flex flex-col justify-between">
-                    <h3 className='text-2xl font-semibold text-gray-900 mb-2'>{review.name}</h3>
+                    <h3 className='text-2xl font-semibold text-gray-900 mb-2'>{name}</h3>
                     <div className='my-3'>
-                        <p className='text-xl text-gray-800'>{review.review}</p>
+                        <p className='text-xl text-gray-800'>{reviewText}</p>
                         <br />
                         <div className="flex">
-                            <p className='text-xl text-gray-700'>Rating: {review.rating} </p>
+                            <p className='text-xl text-gray-700'>Rating: {rating} </p>
                             <div className="w-7 h-7">
                                 <span className='text-yellow-400 text-xl w-full'><StarIcon /></span>
                             </div>
